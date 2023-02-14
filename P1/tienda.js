4ba9aeb7edef0717d1697166bdbf48112c8e509f
dmontero <d.monterog.2018@alumnos.urjc.es>
2023-02-14 12:44:23 +0100

    EMPEZANDO TIENDA

---
+++ P1/tienda.js
@@ -0,0 +1,25 @@
+// PRACTICA 1. TIENDA PROPIA DE COMIDA
+
+// 1.Importamos los modulos url,fs y http para el servidor que nos piden 
+
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+// Creamos el servidor 
+
+const PUERTO = 9000;
+
+const server = http.createServer((req, res) => {
+    console.log("Petición recibida!");
+
+});
+
+// Activamos la escucha y mensaje de activación
+server.listen(PUERTO);
+
+console.log("<====== TIENDA ACTIVADA =====>");
+console.log("<====== MODULOS DE COMIDA CARGADOS =====>");
+console.log("<====== ESCUCHANDO EN EL PUERTO: " + PUERTO + " =====>");
+
+
