63a118681d3e0fb9b8687d6102a5b1f3a179591e
dmontero <d.monterog.2018@alumnos.urjc.es>
2023-02-20 12:22:36 +0100

    cargando html

--- P1/tienda.js
+++ P1/tienda.js
@@ -12,11 +12,48 @@ const url = require('url');
 const PUERTO = 9000;
 const PAGINA = 'tienda_comida.html';
 const ESTILO = 'tienda_comida.css';
+const pagina_error = 'error_404,jpg';
+
+const mine = {
+    "html" : "text/html",
+    "css" : "text/css"
+};
 
 const server = http.createServer((req, res) => {
-    console.log("Petición recibida del cliente!");
-    console.log("Esperando respuesta del servidor...");
+    console.log("<====== TIENDA ACTIVADA =====>");
+    console.log("<====== MODULOS DE COMIDA CARGADOS =====>");
+    console.log("<====== ESCUCHANDO EN EL PUERTO: " + PUERTO + " =====>");
+    console.log("<====== ESPERANDO CLIENTE =====>");
 
+    let myURL = new URL(req.url, 'http://' + req.headers['host']);
+    console.log("La URL del recurso es: " + myURL.href);
+    
+    let cliente = "";
+    console.log(myURL.pathname);
+    if(myURL.pathname == '/'){
+        cliente += PAGINA;
+        console.log(cliente);
+        statusCode = 200;
+        fs.readFile(cliente, (cliente, Page) => {
+            res.writeHead(200, {'Content-Type': mine});
+            res.write(Page);
+            res.end();
+        });
+        
+        
+        
+    } //else if (url.pathname != '/') {
+        //code = 404;
+        //code_msg = "Not Found";
+        //console.log("ERROR")
+        //page = pagina_error;
+   // }
+    
+
+// Sacamos el recurso de los archivos solicitados
+
+recurso = cliente.split(".")[1];
+console.log("Recurso solicitado por el cliente: " + cliente);
 
 });
 
@@ -26,5 +63,7 @@ server.listen(PUERTO);
 console.log("<====== TIENDA ACTIVADA =====>");
 console.log("<====== MODULOS DE COMIDA CARGADOS =====>");
 console.log("<====== ESCUCHANDO EN EL PUERTO: " + PUERTO + " =====>");
+console.log("<====== ESPERANDO CLIENTE =====>");
+
 
 
