56e74d7239d3bcfc1ef44cb0078f752bc50cbb3e
dmontero <d.monterog.2018@alumnos.urjc.es>
2023-02-21 13:34:47 +0100

    funcionando css y gtml y solucionado error no leia css

--- P1/tienda.js
+++ P1/tienda.js
@@ -9,21 +9,18 @@ const url = require('url');
 
 // Creamos el servidor 
 
-const PUERTO = 9000;
+const PUERTO = 9001;
 const PAGINA = 'tienda_comida.html';
-const ESTILO = 'tienda_comida.css';
+const ESTILO = 'style.css';
 const pagina_error = 'error_404,jpg';
 
 const mine = {
     "html" : "text/html",
-    "css" : "text/css"
+    "css" : "text/css",
+    "ico" : "image/ico"
 };
 
 const server = http.createServer((req, res) => {
-    console.log("<====== TIENDA ACTIVADA =====>");
-    console.log("<====== MODULOS DE COMIDA CARGADOS =====>");
-    console.log("<====== ESCUCHANDO EN EL PUERTO: " + PUERTO + " =====>");
-    console.log("<====== ESPERANDO CLIENTE =====>");
 
     let myURL = new URL(req.url, 'http://' + req.headers['host']);
     console.log("La URL del recurso es: " + myURL.href);
@@ -32,43 +29,69 @@ const server = http.createServer((req, res) => {
     console.log(myURL.pathname);
     if(myURL.pathname == '/'){
         cliente += PAGINA;
-        console.log(cliente);
+        console.log("Solicitado por el cliente: " + cliente);
         statusCode = 200;
         fs.readFile(cliente, (error, page) => {
             if (error) {
                 
                 res.writeHead(404, {'Content-Type': mine});
                 return res.end("404 Not Found");
-            }
+                
+            }else{
 
             res.writeHead(200, {'Content-Type': mine});
+            console.log("200 OK");
+            console.log("Enviando pagina principal al cliente");
             res.write(page);
             res.end();
+            }
+        
         });
         
         
+    } else if (myURL.pathname == "/style.css") {
+        cliente += ESTILO;
+        console.log("Solicitado CSS: " + cliente);
+        fs.readFile(cliente, (error, page) => {
+            if (error) {
+                
+                res.writeHead(404, {'Content-Type': mine});
+                return res.end("404 Not Found");
+                
+            }else{
+
+            res.writeHead(200, {'Content-Type': mine});
+            console.log("200 OK");
+            console.log("Enviando CSS al cliente");
+            res.write(page);
+            res.end();
+            }
         
+        });
+
     } else if(myURL.pathname == "/favicon.ico") {
-        console.log("No existe icono pequeño");
+        file = 'favicon.ico'
+        console.log("ENTRAS?");
+        res.writeHead(200, {'Content-Type': mine});
+        console.log("200 OK")
 
 
     } else {
         code = 404;
         code_msg = "Not Found";
-        console.log("ERROR")
+        console.log("ERROR");
         page = pagina_error;
     }
     
 
 // Sacamos el recurso de los archivos solicitados
 
-recurso = cliente.split(".")[1];
-console.log("Recurso solicitado por el cliente: " + cliente);
+//recurso = cliente.split(".")[1];
+//console.log("Recurso solicitado por el cliente: " + cliente);
 
 });
 
 // Activamos la escucha y mensaje de activación
-server.listen(PUERTO);
 
 console.log("<====== TIENDA ACTIVADA =====>");
 console.log("<====== MODULOS DE COMIDA CARGADOS =====>");
@@ -76,4 +99,5 @@ console.log("<====== ESCUCHANDO EN EL PUERTO: " + PUERTO + " =====>");
 console.log("<====== ESPERANDO CLIENTE =====>");
 
 
+server.listen(PUERTO);
 
